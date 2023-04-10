952c6a2245dba8ac6a9a4aa71b25fc7cf53903e7
Harshit Kumar Gupta <harshitkg1234@gmail.com>
2023-04-10 22:04:25 +0530

    code added
    prod level-1

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+//schema design
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please enter your name'],
+        },
+        email: {
+            type: String,
+            required: [true, 'Please enter your email'],
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Please enter your password'],
+        },
+    },
+    { timestamps: true }
+);
+
+//exporting the model
+const userModel = mongoose.model('users', userSchema);
+module.exports = userModel;
